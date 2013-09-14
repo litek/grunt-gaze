546da869f0e7f9ccd06ae020d195b1c413a9a381
Litek <ketil@syscom.as>
2013-09-14 20:32:54 +0200

    added gaze-server

--- tasks/gaze.js
+++ tasks/gaze.js
@@ -1,10 +1,57 @@
 'use strict';
 var path = require("path"),
+    spawn = require("child_process").spawn,
     server = require("./lib/server"),
     gaze = require("gaze");
 
 module.exports = function(grunt) {
-  var callbacks = {};
+  var callbacks = {}, servers = {};
+
+  grunt.registerMultiTask("gaze-server", function(target) {
+    var options = grunt.util._.defaults(this.data, {
+      cmd: process.argv[0],
+      args: [],
+      background: true,
+      cwd: process.cwd(),
+      env: {}
+    });
+
+    if (typeof(options.args) === "string") {
+      options.args = options.args.split(" ");
+    }
+
+    var done = this.async();
+
+    // kill if already running
+    if (servers[target]) {
+      grunt.log.writeln("Restarting server %s %s", options.cmd, options.args.join(" "));
+      servers[target].kill("SIGTERM");
+    }
+
+    // spawn with default environment variables
+    grunt.util._.extend(options.env, process.env);
+
+    var proc = servers[target] = spawn(options.cmd, options.args, {
+      cwd: options.cwd,
+      env: options.env
+    });
+
+    proc.stdout.on("data", function(data) {
+      grunt.log.writeln(data);
+    });
+
+    proc.stderr.on("data", function(data) {
+      grunt.log.warn(data);
+    });
+
+    proc.on("close", function(code) {
+      grunt.log.writeln("Process %s %s exited with code %d", options.cmd, options.args.join(" "), code);
+    });
+
+    if (options.background) {
+      done();
+    }
+  });
 
   grunt.registerTask("gaze", function(target) {
     var key = target || "*", done = this.async();
@@ -16,8 +63,14 @@ module.exports = function(grunt) {
     callbacks[key] = done;
     var targets = grunt.config(this.name);
     var options = this.options({
-      livereload: false
+      livereload: false,
+      server: false
     });
+
+    // spawn server?
+    if (options.server) {
+
+    }
 
     var watch = [], match = [], cwd = process.cwd();
 
